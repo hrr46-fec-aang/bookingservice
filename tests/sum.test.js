72472b453181cf122aaf7eb8ec360f8b9951f01b
area363 <kidonseo@gmail.com>
2020-06-17 22:35:35 -0700

    fixed test

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -5,5 +5,9 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('adds 3 + 2 to equal 5', () => {
-  expect(sum(3, 2)).toBe(3);
+  expect(sum(3, 2)).toBe(5);
+});
+
+test('adds 3 + 4 to equal 7', () => {
+  expect(sum(3, 4)).toBe(7);
 });
