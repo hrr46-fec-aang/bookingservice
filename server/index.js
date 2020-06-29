bd58f92479cf2917e327bab822075ad032e76214
area363 <kidonseo@gmail.com>
2020-06-29 16:11:03 -0700

    add cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }))
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(express.static(__dirname + '/../dist'));
 app.use('/:id', express.static(__dirname + '/../dist'));
 
